f4951b37568367a921014541764b045cd0d0605b
UltraViolet33 <ulysse.valdenairepro@gmail.com>
2022-04-06 09:25:08 +0200

    msg error login

--- app/controller/Login.php
+++ app/controller/Login.php
@@ -6,10 +6,15 @@ class Login extends Controller
 {
     public function index()
     {
+        $_SESSION['error'] = "";
+
+
         if (isset($_POST['login'])) {
+            var_dump($_POST);
             $userManager = $this->loadModel("User");
 
             if (empty($_POST['email'])) {
+
                 $_SESSION['error'] = "Email manquant</br>";
             }
 
@@ -17,12 +22,16 @@ class Login extends Controller
                 $_SESSION['error'] .= "Mot de passe manquant</br>";
             }
 
-            $email = validateData($_POST['email']);
-            $data = $userManager->getUser($email, $_POST['password']);
-            if ($data && is_object($data[0])) {
-                $this->userToSession($data[0]);
-                header("Location: home");
-                die;
+            if ($_SESSION['error'] === "") {
+                $email = validateData($_POST['email']);
+                $data = $userManager->getUser($email, $_POST['password']);
+                if ($data && is_object($data[0])) {
+                    $this->userToSession($data[0]);
+                    header("Location: home");
+                    die;
+                } else {
+                    $_SESSION['error'] .= "Mot de passe ou email invalide <br>";
+                }
             }
         }
         $this->view("login");
