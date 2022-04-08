a7551811ef48e5b1758caa6310f4cac1cf709776
UltraViolet33 <ulysse.valdenairepro@gmail.com>
2022-04-08 16:05:43 +0200

    delete categories and posts

--- app/view/admin/categories.php
+++ app/view/admin/categories.php
@@ -6,24 +6,20 @@
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">Titre</th>
-                <th scope="col">Date</th>
-                <!-- <th scope="col">Categories</th> -->
                 <th scope="col">Modifier</th>
                 <th scope="col">Supprimer</th>
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($data['limitPosts'] as $post) : ?>
+            <?php foreach ($data['limitCategories'] as $category) : ?>
                 <tr>
-                    <th scope="row"><?= $post->id ?></th>
-                    <td><a href="<?= ROOT ?>post/details/<?= $post->id ?>" class="btn btn-primary"><?= validateData($post->name) ?></a></td>
-                    <td><?= validateData($post->created_at) ?></td>
-             
+                    <th scope="row"><?= $category->id ?></th>
+                    <td><a href="<?= ROOT ?>post/category/<?= $category->id ?>" class="btn btn-primary"><?= validateData($category->name) ?></a></td>
                     <td><a class="btn btn-primary">Modifier</a></td>
                     <td>
-                        <form onsubmit="return confirm('Voulez vous supprimer cet article ?')" action="<?= ROOT ?>admin/posts/delete/<?= $post->id ?>" method="POST">
+                        <form onsubmit="return confirm('Voulez vous supprimer cette catégorie ?')" action="<?= ROOT ?>admin/categories/delete/<?= $category->id ?>" method="POST">
                             <input hidden="hidden" name="token" value="<?=$_SESSION['token'] ?>">
-                            <button  type="submit" name="deletePost" class="btn btn-warning">Supprimer</button>
+                            <button  type="submit" name="deleteCategories" class="btn btn-warning">Supprimer</button>
                         </form>
                     </td>
                 </tr>
