37ace0a2ad28e3ffcd9ba5beac64c44bbb35f8a6
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-04-24 12:44:12 +0200

    404

---
+++ app/view/404.php
@@ -0,0 +1,8 @@
+<?php $this->view("inc/header", $data); ?>
+<div class="row-justify-content-center">
+    <div class="col-12 text-center ">
+        <h1>404</h1>
+        <h1>Not Found</h1>
+    </div>
+</div>
+<?php $this->view("inc/footer", $data); ?>
