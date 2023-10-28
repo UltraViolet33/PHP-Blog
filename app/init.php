71050250fbf40eef56dfb8e6b972c0b70d883342
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-10-28 19:27:33 +0200

    remove useless init file

--- app/init.php
+++ app/init.php
@@ -1,8 +1,8 @@
 <?php
 
 // include "../vendor/autoload.php";
-include "../app/core/config.php";
-include "../app/core/functions.php";
-include "../app/core/Controller.php";
-include "../app/core/Database.php";
-include "../app/core/App.php";
+// include "../app/core/config.php";
+// include "../app/core/functions.php";
+// include "../app/core/Controller.php";
+// include "../app/core/Database.php";
+// include "../app/core/App.php";
